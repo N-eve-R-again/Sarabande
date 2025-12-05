54ff86a09c646a45fb68ab818f0e1ca991ca357d
N-eve-R-again <geminitaurus.pro@gmail.com>
2025-12-05 12:32:39 +0100

    Misc & Feedbacks
    juste quelques ajustements de feedback, et modifs visuelles.

--- Assets/Dev/Scripts/Gameplay/Visuals/CardinalSpriteVisual.cs
+++ Assets/Dev/Scripts/Gameplay/Visuals/CardinalSpriteVisual.cs
@@ -148,6 +148,7 @@ namespace Sarabande.Visuals
 
         private void FixRotation()
         {
+            return;
             if (!_sr) return;
             float parentYaw = transform.eulerAngles.y;
             _sr.transform.localRotation = Quaternion.Euler(-90f, -parentYaw, 0f);
